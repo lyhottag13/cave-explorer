b04cec660a5cd2c73db78fca5e701b813a002ca7
lyhottag13 <leonardo@promoevt.com>
2026-07-20 01:57:05 -0700

    Added Quit Script

--- utils.gd
+++ utils.gd
@@ -1,4 +1,8 @@
 extends Node
 
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("escape"):
+		get_tree().quit()
+
 func sleep(time: float) -> Signal:
 	return get_tree().create_timer(time).timeout
