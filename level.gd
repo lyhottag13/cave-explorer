a19f7a7496af6dfc84e66b70d339e8a9234da938
lyhottag13 <leonardo@promoevt.com>
2026-07-20 00:15:59 -0700

    Added Sprites and Dynamite/Debris Cutscene

--- level.gd
+++ level.gd
@@ -1,7 +1,7 @@
 class_name Level extends Node2D
 
 # Stores the connections for a certain room in a {direction: room number} format.
-var level_connections: Dictionary = {}
+var connections: Dictionary = {}
 var text: String
 
 @onready var background: AnimatedSprite2D = $Background
@@ -10,14 +10,36 @@ var text: String
 @onready var left_sprite: Sprite2D = $Left
 @onready var middle_sprite: Sprite2D = $Middle
 @onready var right_sprite: Sprite2D = $Right
+@onready var debris: Sprite2D = $Debris
+@onready var locked_door: AnimatedSprite2D = $LockedDoor
+@onready var dynamite: AnimatedSprite2D = $Dynamite
 
-func setup(connections: Dictionary) -> void:
-	level_connections = connections
-	left_sprite.visible = connections.has("left")
-	middle_sprite.visible = connections.has("forward")
-	right_sprite.visible = connections.has("right")
-	if connections.has("creepy"):
+func setup(p_connections: Dictionary, p_properties: Array) -> void:
+	connections = p_connections
+	left_sprite.visible = p_connections.has("left")
+	middle_sprite.visible = p_connections.has("forward")
+	right_sprite.visible = p_connections.has("right")
+	if p_properties.has("creepy"):
 		play_creepy()
+	debris.visible = p_properties.has("debris")
+	locked_door.visible = p_properties.has("locked_door")
+	dynamite.visible = p_properties.has("dynamite")
 
 func play_creepy() -> void:
 	background.animation = "creepy"
+
+func open_door() -> void:
+	locked_door.animation = "open"
+
+func hide_debris() -> void:
+	debris.hide()
+
+func hide_dynamite() -> void:
+	dynamite.hide()
+
+func show_dynamite() -> void:
+	dynamite.show()
+	dynamite.animation = "lit"
+	await dynamite.animation_finished
+	print("Hey!")
+	dynamite.hide()
--- scenes/game.gd
+++ scenes/game.gd
@@ -5,7 +5,7 @@ const LEVEL = preload("uid://jmcyftqrnl7m")
 
 var inventory: Array[String] = []
 
-var room_data: Array[Dictionary] = [
+var room_connections: Array[Dictionary] = [
 	{forward = 1},
 	{back = 0, forward = 2},
 	{back = 1, forward = 4, right = 3},
@@ -16,6 +16,19 @@ var room_data: Array[Dictionary] = [
 	{right = 6},
 	{left = 5},
 	{creepy = true}
+]
+
+var room_properties: Array[Array] = [
+	[],
+	[],
+	["debris"],
+	["dynamite"],
+	[],
+	["locked_door"],
+	[],
+	[],
+	[],
+	["creepy"]
 ]
 
 var room_text: Array[String] = [
@@ -49,17 +62,17 @@ var is_puzzle_solved := false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	for room in room_data:
+	for i in len(room_connections):
 		var new_room := LEVEL.instantiate()
 		add_child(new_room)
 		new_room.hide()
-		new_room.setup(room)
+		new_room.setup(room_connections[i], room_properties[i])
 		rooms.append(new_room)
 	rooms[current_room_index].show()
 	draw_buttons(current_room_index)
 
 func draw_buttons(room_index: int):
-	var room = room_data[room_index]
+	var room = room_connections[room_index]
 	forward.visible = room.has("forward")
 	left.visible = room.has("left")
 	right.visible = room.has("right")
@@ -92,30 +105,33 @@ func switch_room(new_room_index: int):
 
 # Below functions are VERY FRAGILE because if there's a button drawn for a direction that does not exist, the code will BREAK.
 func _on_forward_pressed() -> void:
-	var new_room_index = room_data[current_room_index]["forward"]
+	var new_room_index = room_connections[current_room_index]["forward"]
 	switch_room(new_room_index)
 
 func _on_right_pressed() -> void:
-	var new_room_index = room_data[current_room_index]["right"]
+	var new_room_index = room_connections[current_room_index]["right"]
 	switch_room(new_room_index)
 
 func _on_left_pressed() -> void:
-	var new_room_index = room_data[current_room_index]["left"]
+	var new_room_index = room_connections[current_room_index]["left"]
 	switch_room(new_room_index)
 
 func _on_back_pressed() -> void:
-	var new_room_index = room_data[current_room_index]["back"]
+	var new_room_index = room_connections[current_room_index]["back"]
 	switch_room(new_room_index)
 
 func _on_menu_act() -> void:
 	if current_room_index == 3 && not inventory.has("dynamite"):
+		rooms[3].hide_dynamite()
 		inventory.append("dynamite")
 		print("got!")
 		room_text[current_room_index] = "You got the dynamite!"
 		room_text[2] = "This looks bombable..."
 		menu.show_text(room_text[current_room_index])
-	elif current_room_index == 2 && inventory.has("dynamite"):
+	elif current_room_index == 2 && inventory.has("dynamite") && not is_debris_cleared:
 		is_debris_cleared = true
+		await rooms[2].show_dynamite()
+		rooms[2].hide_debris()
 		print("kaboom!")
 		room_text[current_room_index] = "The debris is cleared!"
 		menu.show_text(room_text[current_room_index])
@@ -130,3 +146,5 @@ func _on_puzzle_menu_combo_checked(correct: bool) -> void:
 	if is_puzzle_solved:
 		room_text[current_room_index] = "The door swings open!"
 		menu.show_text(room_text[current_room_index])
+		rooms[5].open_door()
+		
